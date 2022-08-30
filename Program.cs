8d7b0f9d114a214193c826091f64c6f38b3fb17b
Ajay Pradhan <ajay.pradhan@metacube.com>
2022-08-30 12:12:13 +0530

    feature: extension method

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringUtility;
+
+public static class StringUtility
+{
+    public static string getTrimmed(this string str)
+    {
+        return str.Trim();
+    }
+}
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        try
+        {
+            string? input = Console.ReadLine();
+            if(input == null) {
+                throw new ArgumentNullException();
+            }
+            Console.WriteLine("Actual text: \"" + input + "\"");
+            Console.WriteLine("After trim: \"" + input.getTrimmed() + "\"");
+
+        }catch (Exception e) {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
